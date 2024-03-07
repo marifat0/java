72ba62c2a6a58c2cc79375c827cf4d7c08e1f7e4
hanif <marifatalhanif@gmail.com>
2024-03-07 16:14:14 +0700

    adding Tapestry ft. Spring

---
+++ demoapp/src/main/java/com/tpstrysprng/demoapp/pages/Index.java
@@ -0,0 +1,14 @@
+package com.tpstrysprng.demoapp.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application demoapp.
+ */
+public class Index
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
