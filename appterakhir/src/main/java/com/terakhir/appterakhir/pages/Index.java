a9124c11c96439460bd52f642fbef8f1d0361faf
hanif <marifatalhanif@gmail.com>
2024-03-13 12:44:21 +0700

    Initial commit

---
+++ appterakhir/src/main/java/com/terakhir/appterakhir/pages/Index.java
@@ -0,0 +1,14 @@
+package com.terakhir.appterakhir.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application appterakhir.
+ */
+public class Index
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
